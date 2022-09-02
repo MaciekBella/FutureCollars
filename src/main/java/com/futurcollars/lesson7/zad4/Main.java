9d435784e6ddbf65ef3d3ab0176fb4208b179399
Maciek <maciejbella@op.pl>
2022-09-02 15:44:40 +0200

    dodano zadanie 4

---
+++ src/main/java/com/futurcollars/lesson7/zad4/Main.java
@@ -0,0 +1,9 @@
+package com.futurcollars.lesson7.zad4;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int resultConverterMinuteToSecond = ConvertMinuteToSecond.getConverterMinutesToSecond(15);
+        System.out.println(resultConverterMinuteToSecond);
+    }
+}
