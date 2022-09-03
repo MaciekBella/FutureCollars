5099a7b4d5464b678d14e52000b5672592b2ec30
Maciek <maciejbella@op.pl>
2022-09-03 14:30:58 +0200

    Revert "dodano zadanie 4"
    This reverts commit 285866131e5da5af212b1cd88166b7c38f8017c0.

--- src/main/java/com/futurcollars/lesson8/zad4/FirstText.java
+++
@@ -1,9 +0,0 @@
-package com.futurcollars.lesson8.zad4;
-
-public class FirstText implements TextFormatter {
-
-    @Override
-    public String formatText(String next) {
-        return next.toUpperCase();
-    }
-}
--- src/main/java/com/futurcollars/lesson8/zad4/TextFormatter.java
+++
@@ -1,6 +0,0 @@
-package com.futurcollars.lesson8.zad4;
-
-public interface TextFormatter {
-
-    String formatText(String next);
-}
