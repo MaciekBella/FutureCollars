507ffdf9fce4e342c0d42ca57b288a014a6b45fa
Maciek <maciejbella@op.pl>
2022-09-03 14:42:34 +0200

    dodano zadanie 8

---
+++ src/main/java/com/futurcollars/lesson8/zad8/Main.java
@@ -0,0 +1,12 @@
+package com.futurcollars.lesson8.zad8;
+
+public class Main {
+    public static void main(String[] args) {
+        Person kamil = new Person("Kamil", 18, 180, "Transport");
+        Person maciek = new Person("Kamil", 18, 180, "Transport");
+        Person andrzej = new Person("Andrzej", 24, 179, "Programista");
+
+        System.out.println(kamil.hashCode(maciek));
+        System.out.println(maciek.equals(andrzej));
+    }
+}
---
+++ src/main/java/com/futurcollars/lesson8/zad8/Person.java
@@ -0,0 +1,35 @@
+package com.futurcollars.lesson8.zad8;
+
+import java.util.Objects;
+
+public class Person {
+    private String name;
+    private int age;
+    private int height;
+    private String work;
+
+    public Person(String name, int age, int height, String work) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+        this.work = work;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age && height == person.height && Objects.equals(name, person.name) && Objects.equals(work, person.work);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age, height, work);
+    }
+
+    public boolean hashCode(Person name) {
+        return false || true;
+    }
+
+}
