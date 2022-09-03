1e6d18e7528ccbc5bdeb0ff8847d89d755dc0733
Maciek <maciejbella@op.pl>
2022-09-03 14:52:24 +0200

    dodano zadanie 9

---
+++ src/main/java/com/futurcollars/lesson8/zad9/Main.java
@@ -0,0 +1,8 @@
+package com.futurcollars.lesson8.zad9;
+
+public class Main {
+    public static void main(String[] args) {
+        Triangle triangle = new Triangle(6, 8);
+        System.out.println(triangle.toString());
+    }
+}
