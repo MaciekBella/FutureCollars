eb216ae20eed5d806c0dcfce30f95dc24bb61136
Maciek <maciejbella@op.pl>
2022-09-03 14:04:00 +0200

    dodano zadanie 1

---
+++ src/main/java/com/futurcollars/lesson8/zad1/ConsoleMassage.java
@@ -0,0 +1,12 @@
+package com.futurcollars.lesson8.zad1;
+
+public class ConsoleMassage extends Massage {
+
+    public ConsoleMassage(){
+
+    }
+    @Override
+    public String getMassage() {
+        return "This is class ConsoleMassage";
+    }
+}
---
+++ src/main/java/com/futurcollars/lesson8/zad1/WindowMassage.java
@@ -0,0 +1,12 @@
+package com.futurcollars.lesson8.zad1;
+
+public class WindowMassage extends Massage {
+
+    public WindowMassage(){
+
+    }
+    @Override
+    public String getMassage() {
+        return "This is class WindowMassage";
+    }
+}
