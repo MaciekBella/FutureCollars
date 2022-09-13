86896311f23250a2cf6ff16d54291136a6aade9b
Maciek <maciejbella@op.pl>
2022-09-13 15:32:11 +0200

    poprawiono  zadanie 4

--- src/main/java/com/futurcollars/lesson7/zad4/ConvertMinuteToSecond.java
+++
@@ -1,9 +0,0 @@
-package com.futurcollars.lesson7.zad4;
-
-public class ConvertMinuteToSecond {
-    public static int getConverterMinutesToSecond(int minute) {
-
-        return minute * 60;
-    }
-
-}
--- src/main/java/com/futurcollars/lesson7/zad4/Main.java
+++ src/main/java/com/futurcollars/lesson7/zad4/Main.java
@@ -3,7 +3,7 @@ package com.futurcollars.lesson7.zad4;
 public class Main {
     public static void main(String[] args) {
 
-        int resultConverterMinuteToSecond = ConvertMinuteToSecond.getConverterMinutesToSecond(15);
+        int resultConverterMinuteToSecond = TimeConverter.convertMinutesToSeconds(15);
         System.out.println(resultConverterMinuteToSecond);
     }
 }
