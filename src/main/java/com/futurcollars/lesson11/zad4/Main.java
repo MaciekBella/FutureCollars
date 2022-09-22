d78578fb39ba755cffb4dd5c776352a931c44247
Maciek <maciejbella@op.pl>
2022-09-22 18:23:10 +0200

    dodano zadanie 4

---
+++ src/main/java/com/futurcollars/lesson11/zad4/Main.java
@@ -0,0 +1,8 @@
+package com.futurcollars.lesson11.zad4;
+
+public class Main {
+    public static void main(String[] args) {
+        NameMonth month1 = new NameMonth();
+        System.out.println(month1.getNameMonth(6));
+    }
+}
