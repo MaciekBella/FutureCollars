d11e68350a5c8205788b01d9c17cc6bef51dd929
Maciek <maciejbella@op.pl>
2022-09-22 18:16:05 +0200

    dodano zadanie 1

---
+++ src/main/java/com/futurcollars/lesson11/zad1/Main.java
@@ -0,0 +1,10 @@
+package com.futurcollars.lesson11.zad1;
+
+public class Main {
+    public static void main(String[] args) {
+        DivisibleNumber divisibleNumber = new DivisibleNumber();
+        System.out.println(divisibleNumber.isDivisibleNumber(8));
+        SumOfGivenNumber sumOfGivenNumber = new SumOfGivenNumber();
+        System.out.println(sumOfGivenNumber.getSumOfGivenNumber(896));
+    }
+}
---
+++ src/test/java/com/futurecollars/lesson11/zad1/DivisibleNumberTest.java
@@ -0,0 +1,32 @@
+package com.futurecollars.lesson11.zad1;
+
+import com.futurcollars.lesson11.zad1.DivisibleNumber;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class DivisibleNumberTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {4, 8, 6, 12, 18})
+    void shouldGetDivisibleNumber(int input) {
+        // given
+        DivisibleNumber divisibleNumber = new DivisibleNumber();
+        // when
+        boolean result = divisibleNumber.isDivisibleNumber(input);
+        // then
+        Assertions.assertTrue(result);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"2:true", "4:true", "12:true"}, delimiter = ':')
+    void shouldGetDivisibleNumber(int input, boolean expected) {
+        // given
+        DivisibleNumber divisibleNumber = new DivisibleNumber();
+        // when
+        boolean result = divisibleNumber.isDivisibleNumber(input);
+        // then
+        Assertions.assertEquals(expected, result);
+    }
+}
