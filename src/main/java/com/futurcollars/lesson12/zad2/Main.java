9d6fab483ae2035b0f281ca90aabcb43023c4e62
Maciek <maciejbella@op.pl>
2022-10-01 12:48:32 +0200

    poprawiono zadanie 2

--- src/main/java/com/futurcollars/lesson12/zad2/Main.java
+++
@@ -1,8 +0,0 @@
-package com.futurcollars.lesson12.zad2;
-
-public class Main {
-    public static void main(String[] args) {
-        MyFirstList myFirstList = new MyFirstList();
-        myFirstList.add(5,11);
-    }
-}
--- src/main/java/com/futurcollars/lesson12/zad2/MyFirstList.java
+++ src/main/java/com/futurcollars/lesson12/zad2/MyFirstList.java
@@ -1,51 +1,65 @@
 package com.futurcollars.lesson12.zad2;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class MyFirstList implements OwnList {
 
-    Integer[] numberTab = {1, 23, 42, 526, 74, 2, 243, 3};
-    List<Integer> numberList = new ArrayList<>(Arrays.asList(numberTab));
+    private static final int DEFAULT_CAPACITY = 10;
+    private int size;
+    private Integer[] elements;
+
+    private MyFirstList() {
+        elements = new Integer[DEFAULT_CAPACITY];
+        size = 0;
+    }
 
     @Override
     public int size() {
-        return numberList.size();
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return numberList.isEmpty();
+        return size == 0;
     }
 
     @Override
     public void add(Integer element) {
-        numberList.add(element);
+        if (size == elements.length) {
+            Integer[] newElements = Arrays.copyOf(elements, size + 1);
+            newElements[size + 1] = element;
+            this.elements = newElements;
+        } else {
+            elements[size + 1] = element;
+        }
+        size++;
     }
 
     @Override
     public Integer get(int i) {
-        return numberList.get(i);
+        if (i >= size || i < 0)
+            throw new IndexOutOfBoundsException();
+        return elements[i];
     }
 
     @Override
     public void add(int index, Integer element) {
-        if (index > numberList.size()) {
-            throw new ArrayIndexOutOfBoundsException("niema takiego indexu");
-        } else {
-            numberList.add(index, element);
-        }
+        get(index);
+        System.arraycopy(this.elements, index, elements, index + 1, size - index);
+        elements[index] = element;
+        this.size++;
     }
 
     @Override
     public Integer remove(int index) {
-        int result;
-        if (index > numberList.size()) {
-            throw new ArrayIndexOutOfBoundsException("niema takiego indexu");
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException();
         } else {
-            result = numberList.remove(index);
+            Integer item = elements[index];
+            int numElts = elements.length - (index + 1);
+            System.arraycopy(elements, index + 1, elements, index, numElts);
+            size--;
+            return item;
         }
-        return result;
     }
 }
