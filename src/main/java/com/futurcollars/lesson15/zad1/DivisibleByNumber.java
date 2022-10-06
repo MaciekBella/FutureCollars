a29169311326c4123a0d604f37a40bb9b0a3217f
Maciek <maciejbella@op.pl>
2022-10-06 15:45:32 +0200

    poprawiono zadanie 1

--- src/main/java/com/futurcollars/lesson15/zad1/DivisibleByNumber.java
+++ src/main/java/com/futurcollars/lesson15/zad1/DivisibleByNumber.java
@@ -1,13 +1,15 @@
 package com.futurcollars.lesson15.zad1;
 
-import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class DivisibleByNumber {
-    public Collection<Integer> GetARangeOfNumbersDivisibleByThree(int start, int end) {
+
+    public List<Integer> getARangeOfNumbersDivisibleByThree(int start, int end) {
         return IntStream.range(start, end)
                 .filter(n -> n % 3 == 0)
-                .
-
+                .boxed()
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/com/futurcollars/lesson15/zad1/Main.java
+++ src/main/java/com/futurcollars/lesson15/zad1/Main.java
@@ -1,12 +1,12 @@
 package com.futurcollars.lesson15.zad1;
 
-import java.util.stream.IntStream;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
-        IntStream intStream = IntStream.of(5, 6, 9, 16, 15, 18, 13, 19, 30, 36);
         DivisibleByNumber divisibleByNumber = new DivisibleByNumber();
-        divisibleByNumber.GetARangeOfNumbersDivisibleByThree(intStream);
+        List<Integer> numberList = divisibleByNumber.getARangeOfNumbersDivisibleByThree(54, 645);
+        numberList.stream().forEach(System.out::println);
     }
 }
