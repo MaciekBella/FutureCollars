39fd0ccc64036394b3461e43fb2a6ac8f17bf087
Maciek <maciejbella@op.pl>
2022-10-09 13:13:34 +0200

    dodano zadanie 5

---
+++ src/main/java/com/futurcollars/lesson14/zad5/PasswordValidator.java
@@ -0,0 +1,13 @@
+package com.futurcollars.lesson14.zad5;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PasswordValidator {
+
+    public boolean isValidatePassword(String password) {
+        Pattern pattern = Pattern.compile("^(?!.*2021)(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{7,20}$");
+        Matcher matcher = pattern.matcher(password);
+        return matcher.matches();
+    }
+}
